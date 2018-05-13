23d2821a133f2b0cfe1bdba76adaa27bef22e389
haykam821 <haykam821@users.noreply.github.com>
2018-05-13 15:51:27 -0400

    Tidy and fix the forEachAsync thing

--- constructor.js
+++ constructor.js
@@ -12,65 +12,65 @@
 		}
 	}
 
-    class Pixel {
-        constructor(x, y, color) {
-            this.position = {
-                x: x,
-                y: y,
-            };
-            this.color = color;
-        }
-        place() {
-            App.color(this.color);
-            App.place(this.position.x, this.position.y);
-        }
-    }
-    class PixelBuild {
-        constructor(x, y, map) {
-            this.position = {
-                x: x,
-                y: y,
-            };
+	class Pixel {
+		constructor(x, y, color) {
+			this.position = {
+				x: x,
+				y: y,
+			};
+			this.color = color;
+		}
+		place() {
+			App.color(this.color);
+			App.place(this.position.x, this.position.y);
+		}
+	}
+	class PixelBuild {
+		constructor(x, y, map) {
+			this.position = {
+				x: x,
+				y: y,
+			};
 
-            this.map = map.map((row, yIndex) => {
-                return row.map((pixel, xIndex) => {
-                    return new Pixel(x + xIndex, y + yIndex, pixel);
-                });
-            });
-        }
-        allPixels() {
-            return [].concat(...this.map);
-        }
-        placeAll() {
-            this.allPixels().forEach(pixel => {
-                pixel.place();
-            });
-        }
-        placeAllTimed() {
-            this.allPixels().forEachAsync(async pixel => {
-                await waitUntil(() => {
-                    return App.cooledDown();
-                });
-                await sleep(1000);
-                pixel.place();
-                console.log("Cooled down!")
-            });
-        }
-    }
+			this.map = map.map((row, yIndex) => {
+				return row.map((pixel, xIndex) => {
+					return new Pixel(x + xIndex, y + yIndex, pixel);
+				});
+			});
+		}
+		allPixels() {
+			return [].concat(...this.map);
+		}
+		placeAll() {
+			this.allPixels().forEach(pixel => {
+				pixel.place();
+			});
+		}
+		placeAllTimed() {
+			forEachAsync(this.allPixels(), async pixel => {
+				await waitUntil(() => {
+					return App.cooledDown();
+				});
+				await sleep(1000);
+				pixel.place();
+				console.log("Cooled down!");
+			});
+		}
+	}
 
-    function setUpGuide() {
-        const x = parseInt(prompt("X position for build?"));
-        const y = parseInt(prompt("Y position for build?"));
-        try {
-            const map = JSON.parse(prompt("A 2D array in JSON (the map) for the build?"));
-            
-            const build = new PixelBuild(x, y, map);
-            build.placeAllTimed();
-        } catch (error) {
-            alert("This JSON is malformed. Use createMap.js to make correct JSON.");
-        }
-    }
-    setUpGuide();
+	function setUpGuide() {
+		const x = parseInt(prompt("X position for build?"));
+		const y = parseInt(prompt("Y position for build?"));
+		try {
+			const map = JSON.parse(prompt("A 2D array in JSON (the map) for the build?"));
+
+			const build = new PixelBuild(x, y, map);
+			build.placeAllTimed();
+		} catch (error) {
+			alert("This JSON is malformed. Use createMap.js to make correct JSON.");
+		}
+	}
+	setUpGuide();
 })();
 
 /*
