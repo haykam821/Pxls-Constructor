c258f90742d8301d99f5f444017bfcc83819edf2
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-10 03:36:53 -0400

    Random mode

--- constructor.js
+++ constructor.js
@@ -52,7 +52,7 @@
 					await waitUntil(() => {
 						return App.cooledDown();
 					}, 120000);
-					// await sleep(randInt(500, 2500));
+					await sleep(randInt(500, 2500));
 					pixel.place();
 					console.log("Cooled down!");
 				}
@@ -61,17 +61,43 @@
 		}
 	}
 
-	function setUpGuide() {
-		const x = parseInt(prompt("X position for build?", 0));
-		const y = parseInt(prompt("Y position for build?", 0));
-		try {
-			const map = JSON.parse(prompt("A 2D array in JSON (the map) for the build? You can generate this with Palette Image Mapper.", "[ [1, 2], [3, 4] ]"));
+	const modes = {
+		build: () => {
+			const x = parseInt(prompt("X position for build?", 0));
+			const y = parseInt(prompt("Y position for build?", 0));
+			try {
+				const map = JSON.parse(prompt("A 2D array in JSON (the map) for the build? You can generate this with Palette Image Mapper.", "[ [1, 2], [3, 4] ]"));
 
-			const build = new PixelBuild(x, y, map);
-			build.placeAllTimed();
-		} catch (error) {
-			alert("This JSON is malformed. Use Palette Image Mapper to make correct JSON.");
+				const build = new PixelBuild(x, y, map);
+				build.placeAllTimed();
+			} catch (error) {
+				alert("This JSON is malformed. Use Palette Image Mapper to make correct JSON.");
+			}
+		},
+		random: () => {
+			const colorPrompt = prompt("Which color pixel? (Color index or 'random')", "random");
+			const board = document.getElementById("board");
+			if (colorPrompt === "random") {
+				setInterval(() => {
+					new Pixel(randInt(board.width), randInt(board.height), randInt(0, 23)).place();
+				});
+			} else {
+				const color = parseInt(colorPrompt);
+				setInterval(() => {
+					new Pixel(randInt(board.width), randInt(board.height), color).place();
+				});
+			}
+		},
+	};
+
+	function setUpGuide() {
+		const type = prompt("Pick a mode: " + Object.keys(modes).join(", "), "build");
+		if (modes[type]) {
+			return modes[type]();
+		} else {
+			alert("That's not a mode.");
 		}
+
 	}
 	setUpGuide();
 })();
